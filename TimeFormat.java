d5277ea56db028c778c5714aea45ae889036135b
Yoav Rubin <yoav.rubin@post.runi.ac.il>
2025-11-04 15:15:38 +0200

    0411

--- TimeFormat.java
+++ TimeFormat.java
@@ -6,7 +6,7 @@ public class TimeFormat {
         // Does the same with the minutes part of the input.
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
          if (hours >= 12) {
-            newhours = hours - 12;
+             int newhours = hours - 12;
             System.out.printf("The time is: %d:%02d PM\n", newhours, minutes);
         } else {
             System.out.printf("The time is: %d:%02d AM\n", hours, minutes);
