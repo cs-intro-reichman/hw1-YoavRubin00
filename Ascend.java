fbea751ec81d7d90ab03b04aa2a7cc188799a9b8
Yoav Rubin <yoav.rubin@post.runi.ac.il>
2025-11-04 12:42:30 +0200

    Completed FVCalc assignment

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,13 @@
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
+    public static void main(String[] args) {
+    double a = Math.random() * 100;
+    double b = Math.random() * 100;
+    double c = Math.random() * 100;
+    double Tempmin = Math.min(a, Math.min(b, c));
+    double Tempmax = Math.max(a, Math.max(b, c));
+    double Tempmid = a + b + c - Tempmin - Tempmax;
 	}
 }
